a5b64f93017d5d40d80080d40be00c38aee3a223
craymahdee142 <craymahdaniel@gamil.com>
2023-10-21 23:02:33 +0000

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['17.206.20.255', '34.73.75.125']
+env.hosts = ['54.157.147.218', '3.85.41.177']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 

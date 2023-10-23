0f650a321967260c77b25a315504b0f95b94ee5a
craymahdee142 <craymahdaniel@gamil.com>
2023-10-23 02:33:58 +0000

    updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def is_number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Display HTML page if n is an ineteger"""
-    return render_temaplate("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
